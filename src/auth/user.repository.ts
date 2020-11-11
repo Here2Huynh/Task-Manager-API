ad86a0c8cae58f3dd56a9b3b3a73f7123d62dab7
John Huynh <jhnhnyh9@gmail.com>
2020-11-10 21:09:23 -0800

    added user password validation

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,7 +6,6 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { throws } from 'assert';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -29,6 +28,19 @@ export class UserRepository extends Repository<User> {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
     }
   }
 
