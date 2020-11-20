0d62dcb2c2a9ef7afbc17ff98bbcb5baa5467c9d
John Huynh <jhnhnyh9@gmail.com>
2020-11-19 23:50:29 -0800

    added jest test for getTaskId

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -3,10 +3,18 @@ import { TasksService } from './tasks.service';
 import { TaskRespository } from './task.repository';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
+import { NotFoundException } from '@nestjs/common';
 
-const mockUser = { username: 'Test User' };
+const mockUser = { id: '12', username: 'Test User' };
+const mockTask = {
+  title: 'Test task',
+  description: 'Test desc',
+};
+const mockCreateTaskDto = { title: 'Test', description: 'with jest' };
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
+  findOne: jest.fn(),
+  createTask: jest.fn(),
 });
 
 describe('TaskService', () => {
@@ -37,7 +45,36 @@ describe('TaskService', () => {
       const result = await taskService.getTasks(filters, mockUser);
       expect(taskRepository.getTasks).toHaveBeenCalled();
       expect(result).toEqual('someValue');
-      // expect taskRepository.getTasks TO HAVE BEEN CALLED
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls taskRepository.findOne() and successfully retrieve and return the task', async () => {
+      taskRepository.findOne.mockResolvedValue(mockTask);
+      const result = await taskService.getTaskById(1, mockUser);
+      expect(result).toEqual(mockTask);
+      expect(taskRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1, userId: mockUser.id },
+      });
+    });
+
+    it('throws an error as task is not found', async () => {
+      taskRepository.findOne.mockResolvedValue(null);
+      expect(taskService.getTaskById(1, mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('createTask', () => {
+    it('creates a task', async () => {
+      taskRepository.createTask.mockResolvedValue('someTask');
+      const result = await taskService.createTask(mockCreateTaskDto, mockUser);
+      expect(taskRepository.createTask).toHaveBeenCalledWith(
+        mockCreateTaskDto,
+        mockUser,
+      );
+      expect(result).toEqual('someTask');
     });
   });
 });
